b32a459b4d46c48f31fb46b57eff61708107dedd
Gerbrand Bosch <gerbrandb@gmail.com>
2022-06-10 14:08:47 +0200

    Cleaned example to contain only the required code and files

---
+++ nl-uzipoc-dot-net-client/Startup.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace nl_uzipoc_dot_net_client
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages(options =>
+            {
+                options.Conventions.AllowAnonymousToPage("/Index");
+            });
+            services.AddSession(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
+            });
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "cookie";
+                options.DefaultSignInScheme = "cookie";
+                options.DefaultChallengeScheme = "oidc";
+            })
+                .AddCookie("cookie")
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.BackchannelHttpHandler = new HttpClientHandler()
+                    {
+                        ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+                        {
+                            return true;
+                        }
+                    };
+                    options.Scope.Clear();
+                    options.Scope.Add("openid");
+                    Console.WriteLine("HIer!!");
+                    Console.WriteLine(options.Scope);
+                    //options.RequireHttpsMetadata = false;
+                    options.Authority = "https://inge_6_uzipoc:8006";
+                    options.ClientId = "nl_uzipoc_dot_net";
+                    options.ResponseType = "code";
+                    options.ResponseMode = "form_post";
+                    options.UsePkce = true;
+                    options.GetClaimsFromUserInfoEndpoint = false;
+                    options.Events = new OpenIdConnectEvents
+                    {
+                        OnTokenResponseReceived = UserInfoTool.FetchUserInfoOnTokenResponseReceived                        
+                    };
+                    
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseSession();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages()
+                .RequireAuthorization();
+                
+            });
+        }
+    }
+}
+
---
+++ nl-uzipoc-dot-net-client/UserInfoTool.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace nl_uzipoc_dot_net_client
+{
+	public class UserInfoTool
+	{
+		public static async Task FetchUserInfoOnTokenResponseReceived(TokenResponseReceivedContext tokenResponseReceivedContext)
+        {
+			var configuration = await tokenResponseReceivedContext.Options.ConfigurationManager.GetConfigurationAsync(CancellationToken.None);
+			var jwksRequestMessage = new HttpRequestMessage(HttpMethod.Get, configuration.JwksUri);
+			var jwksResponse = await tokenResponseReceivedContext.Options.Backchannel.SendAsync(jwksRequestMessage);
+			var jwks = await jwksResponse.Content.ReadAsStringAsync();
+			var jsonWebKeySet = JsonWebKeySet.Create(jwks);
+
+
+			var userInfoRequestMessage = new HttpRequestMessage(HttpMethod.Get, configuration.UserInfoEndpoint);
+			var accessToken = tokenResponseReceivedContext.TokenEndpointResponse.AccessToken;
+
+			userInfoRequestMessage.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+			var response = await tokenResponseReceivedContext.Options.Backchannel.SendAsync(userInfoRequestMessage);
+			var jwe = await response.Content.ReadAsStringAsync();
+			var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+			var clientPrivateKey = new X509SecurityKey(X509Certificate2.CreateFromPemFile(
+				"Resources/client.cert",
+				"Resources/client.key"
+				));
+
+			jwtSecurityTokenHandler.ValidateToken(jwe, new TokenValidationParameters
+			{
+				TokenDecryptionKey = clientPrivateKey,
+				IssuerSigningKeys = jsonWebKeySet.Keys,
+				ValidateIssuer = false,
+				ValidateAudience = false
+            }, out SecurityToken securityToken);
+			tokenResponseReceivedContext.HttpContext.Session.Set("userinfo", Encoding.UTF8.GetBytes(securityToken.ToString()));
+		}
+	}
+}
