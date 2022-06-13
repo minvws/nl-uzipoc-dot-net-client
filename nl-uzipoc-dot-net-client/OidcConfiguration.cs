473c6125709d4f522e9ae75a217ad438d7c4a456
Gerbrand Bosch <gerbrandb@gmail.com>
2022-06-13 13:30:29 +0200

    Moved hardcoded config to appsettings.json and added README

---
+++ nl-uzipoc-dot-net-client/OidcConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace nl_uzipoc_dot_net_client
+{
+    public class OidcConfiguration
+    {
+        public string Authority { get; set; }
+        public string ClientId { get; set; }
+        public string ClientKeyPath { get; set; }
+        public string ClientCertPath { get; set; }
+    }
+}
